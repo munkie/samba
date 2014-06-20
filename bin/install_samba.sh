8003b5b52fa3584ccbb6939e5f81586ff8db3efd
Mikhail Shamin <munk13@gmail.com>
2014-06-20 15:19:07 +0400

    install_samba.sh to init samba on travis-ci

---
+++ bin/install_samba.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo apt-get update -qq
+sudo apt-get install -y samba
+
+echo 'password' | tee - | sudo smbpasswd -a -s $USER
+
+mkdir -p ~/samba-test
+
+echo '[samba-test]' | sudo tee -a /etc/samba/smb.conf
+echo "path = $HOME/samba-test" | sudo tee -a /etc/samba/smb.conf
+echo 'available = yes' | sudo tee -a /etc/samba/smb.conf
+echo "valid users = $USER" | sudo tee -a /etc/samba/smb.conf
+echo 'read only = no' | sudo tee -a /etc/samba/smb.conf
+echo 'browseable = yes' | sudo tee -a /etc/samba/smb.conf
+echo 'public = yes' | sudo tee -a /etc/samba/smb.conf
+echo 'writable = yes' | sudo tee -a /etc/samba/smb.conf
+
+sudo restart smbd
+testparm -s
--- tests/Samba/Functional/StreamTest.php
+++ tests/Samba/Functional/StreamTest.php
@@ -4,25 +4,82 @@ namespace Samba\Functional;
 
 use Samba\SambaStreamWrapper;
 use Samba\TestCase;
+use FilesystemIterator;
 
 class StreamTest extends TestCase
 {
     /**
      * @var string
      */
-    protected $smbHost;
+    protected static $host;
+
+    /**
+     * @var string
+     */
+    protected static $user;
+
+    /**
+     * @var string
+     */
+    protected static $home;
+
+    /**
+     * @var string
+     */
+    protected static $password = 'password';
+
+    /**
+     * @var string
+     */
+    protected static $url;
+
+    /**
+     * @var
+     */
+    protected static $localPath;
+
+    /**
+     * @var string
+     */
+    protected static $dir = 'samba-test';
 
     /**
      * @var string
      */
     protected $localFolder = '/home/mshamin/';
 
+    public static function setUpBeforeClass()
+    {
+        self::$home = getenv('HOME');
+        self::$user = getenv('USER');
+        self::$host = gethostname();
+        self::$url = 'smb://' . self::$user . ':' . self::$password . '@' . self::$host . '/' . self::$dir;
+        self::$localPath = self::$home . '/' . self::$dir;
+    }
+
     protected function setUp()
     {
         if (!SambaStreamWrapper::is_registered()) {
             SambaStreamWrapper::register();
         }
-        $this->smbHost = 'smb://mshamin:samba@mshamin-ubuntu';
+
+        $this->clearTestDir();
+    }
+
+    protected function clearTestDir()
+    {
+        $testDir = new FilesystemIterator(
+            self::$localPath,
+            FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_FILEINFO
+        );
+        /* @var \SplFileInfo $fileInfo */
+        foreach ($testDir as $fileInfo) {
+            if ($fileInfo->isDir()) {
+                rmdir($fileInfo->getPathname());
+            } else {
+                unlink($fileInfo->getPathname());
+            }
+        }
     }
 
     protected function tearDown()
@@ -32,7 +89,12 @@ class StreamTest extends TestCase
 
     public function testMkDir()
     {
-        $result = mkdir($this->smbHost . '/samba-test/test-dir');
+        $url = self::$url . '/test-dir';
+        $result = mkdir($url);
         $this->assertTrue($result);
+
+        $localPath = self::$localPath . '/test-dir';
+        $this->assertTrue(file_exists($localPath));
+        $this->assertTrue(is_dir($localPath));
     }
 }
